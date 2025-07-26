2254ba0cf2ff6dcd0d6b9a29b73086d8a5e899d6
Natalia Romanovskaia <romanovskaya.natalya@bodysite.ru>
2025-07-26 14:24:28 +0500

    Продолжение врестки

--- script.js
+++ script.js
@@ -1,63 +1,68 @@
 const banner = document.querySelector('.banner');
 const button = banner.querySelector('#main-button');
-const desc = banner.querySelector('.banner__desc');
-const steps = banner.querySelectorAll('.banner__step');
+const desc2 = banner.querySelector('.banner__desc-second');
+const desc3 = banner.querySelector('.banner__desc-third');
+const stepsWrapper = banner.querySelector('.banner__steps');
+const steps = stepsWrapper.querySelectorAll('.banner__step');
 const slide = banner.querySelector('.banner__slide');
 
-
-let isAnimating = false;
-
-function resetSteps() {
+function resetAnimations() {
   steps.forEach(step => {
-    step.classList.add('hidden');
     step.classList.remove('fade-in', 'fade-out');
-    step.style.animationDelay = '';
   });
-}
-
-function resetSlide() {
-  slide.classList.add('hidden');
+  desc2.classList.remove('fade-in', 'fade-out');
+  desc3.classList.remove('fade-in', 'fade-out');
   slide.classList.remove('slide-animate-in', 'slide-animate-out');
 }
 
 function startCycle() {
-  resetSteps();
-  resetSlide();
+  resetAnimations();
 
+  stepsWrapper.classList.remove('d-none');
+  desc2.classList.remove('d-none');
+  desc2.classList.add('fade-in');
+  
   // === Появление шагов по очереди ===
   steps.forEach((step, index) => {
     setTimeout(() => {
-      step.classList.remove('hidden', 'fade-out');
       step.style.animationDelay = '0s';
       step.classList.add('fade-in');
     }, index * 750);
   });
 
+  return
+
   // === Скрыть шаги одновременно + показать слайд ===
-  setTimeout(() => {
+  setTimeout(() => { 
     steps.forEach((step) => {
-      step.classList.remove('fade-in');
+      // step.classList.remove('fade-in');
       step.classList.add('fade-out');
     });
 
+    desc2.classList.add('fade-out');
+
     // Через 0.5s — скрыть шаги и показать слайд
     setTimeout(() => {
-      steps.forEach((step) => {
-        step.classList.add('hidden');
-      });
+      desc2.classList.add('d-none');
+      stepsWrapper.classList.add('d-none');
 
-      slide.classList.remove('hidden', 'slide-animate-out');
+      desc3.classList.remove('d-none');
+      slide.classList.remove('d-none');
+
+      desc3.classList.add('fade-in');
       slide.classList.add('slide-animate-in');
     }, 500);
   }, 5000);
-
+  // return
   // === Через 2s после показа слайда — скрыть слайд и повторить ===
   setTimeout(() => {
-    slide.classList.remove('slide-animate-in');
     slide.classList.add('slide-animate-out');
+    desc3.classList.add('fade-out');
 
     // После исчезновения — перезапуск цикла
     setTimeout(() => {
+      slide.classList.add('d-none');
+      desc3.classList.add('d-none');
       startCycle();
     }, 1000); // 1s = slideOut duration
   }, 9000); // 5.5s (шаги) + 0.5s (fadeOut) + 3s (пауза перед slide-out)
@@ -65,15 +70,14 @@ function startCycle() {
 
 
 button.addEventListener('click', () => {
-  if (!isAnimating) {
-    isAnimating = true;
     button.classList.add('fade-out');
-    desc.classList.remove('hidden');
-    desc.classList.add('fade-in');
 
     // стартуем цикл
     setTimeout(() => {
+      button.classList.add('d-none');
+      stepsWrapper.classList.remove('d-none');
+      desc2.classList.remove('d-none');
+
       startCycle();
-    }, 500);
-  }
+    }, 300);
 });
